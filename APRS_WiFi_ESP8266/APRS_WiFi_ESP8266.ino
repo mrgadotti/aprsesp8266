ba9cb846f6f232c0aa753367d51b78036bdbc1d9
Marcelo R Gadotti <mrgadotti@users.noreply.github.com>
2017-08-13 14:28:30 -0300

    V1.10
    Change how send beacon, fix some bugs.

--- APRS_WiFi_ESP8266/APRS_WiFi_ESP8266.ino
+++ APRS_WiFi_ESP8266/APRS_WiFi_ESP8266.ino
@@ -1,64 +1,45 @@
 /*
-	APRS Spot using ESP8266
-	PP5MGT - Marcelo
-	pp5mgt@qsl.net
+  APRS Spot using ESP8266
+  PP5MGT - Marcelo
+  pp5mgt@qsl.net
+  Version 1.10
+
+  Define your station symbol
+  "-" Home
+  "$" Phone
+  "'" Plane
+  "`" Antenna
+  ">" Car
+  "Z" Windows 95
+  "," Scout
+  "?" Desktop Computer
+  Can you find others symbols on APRS documentation
+
+  Get location on Google Maps:
+  Latitude -27.590348 Longitude -48.519487
+  APRS coordinate converter:
+  http://digined.pe1mew.nl/?How_to:Convert_coordinates
+  Latitude 2735.42S Longitude 048.31.17W
+
 */
 
 /* Library used by ESP8266 */
 #include <ESP8266WiFi.h>
 
 /* Wifi parameters */
-const char* ssid     = "Wi-Fi SSID";
-const char* password = "Wi-Fi Passowrd";
+const char* ssid     = "WiFi SSID";
+const char* password = "WiFi Password";
 
 /* APRS server */
 char SVR_NAME[] = "brazil.d2g.com";
 #define SVR_PORT 14579
 
-/***************************************************************/
-/*      Station parameters                                     */
-/***************************************************************/
-
-/* Define your callsign, passcode*/
-#define callsign "PP5XXX"
-#define passcode "00000"
-
-/*
-    Get location on Google Maps:
-    Latitude -27.590348 Longitude -48.519487
-    APRS coordinate converter:
-    http://digined.pe1mew.nl/?How_to:Convert_coordinates
-    Latitude 2735.42S Longitude 048.31.17W
-*/
-
-/* Define your location */
-#define location "2735.42S/04831.17W"
-
-/*
-    Define your station symbol
-    "-" Home
-    "$" Phone
-    "'" Plane
-    "`" Antenna
-    ">" Car
-    "Z" Windows 95
-    "," Scout
-    "?" Desktop Computer
-    Can you find others symbols on APRS documentation
-*/
-#define sta_symbol "`"
-
-/* Define your comment */
-#define comment "APRS ESP8266 - pp5mgt@qsl.net"
-
 /* Update interval in minutes */
-int REPORT_INTERVAL = 15;
+int REPORT_INTERVAL = 30;
 
 /***************************************************************/
 
-/* Don not change anything bellow */
-
-#define VER "1.01"
+#define VER "1.10"
 #define SVR_VERIFIED "verified"
 
 #define TO_LINE  10000
@@ -68,91 +49,59 @@ int REPORT_INTERVAL = 15;
 // Use WiFiClient class to create TCP connections
 WiFiClient client;
 
-boolean sent; 
-long interval = 0; 
+boolean sent;
+long interval = 0;
 long time_elapsed;
 
 void setup() {
-  Serial.begin(9600);
+  delay(2000);
+
+  Serial.begin(115200);
   delay(5);
 
   pinMode(BT_BEACON, INPUT);
-  
-  Serial.println("-----------------------------------------\n");
-
+  Serial.println("");
   Serial.print("APRS8266 ");
   Serial.println(VER);
 
   // We start by connecting to a WiFi network
   init_wifi();
-
-  // Print user information
-  Serial.print("\nCallsing: ");
-  Serial.println(callsign);
-
-  Serial.print("Location: ");
-  Serial.println(location);
-
-  Serial.print("Report interval in minutes: ");
+  Serial.print("Beacon interval in minutes: ");
   Serial.println(REPORT_INTERVAL);
-
-  Serial.println("\n-----------------------------------------\n");
-
+  Serial.println("");
 }
 
 void loop() {
 
-  sent = false;
-  if ( client.connect(SVR_NAME, SVR_PORT) )
+  /*
+  * Set your tation info here. You can replicate to other stations.
+  */
+  send_beacon("CALLSIGN", "PASSWORD", "0000.00S/00000.00W", "SYMBOL", "COMMENT");
+
+  /*********************************************************************************
+  * Do not change anything below, used for beacon interval
+  */
+  interval = (long)REPORT_INTERVAL * 60L * 1000L;
+  while (time_elapsed < interval)
   {
-    Serial.println("Server connected");
-    client.print("user ");
-    client.print(callsign);
-    client.print(" pass ");
-    client.print(passcode);
-    client.print(" vers APRS8266 ");
-    client.println(VER);
-    if ( wait4content(&client, SVR_VERIFIED, 8) )
+    delay(1);
+    if (!digitalRead(BT_BEACON))
     {
-      Serial.println("Login OK");
-      client.print(callsign);
-      client.print(">APE001,TCPIP*,qAC,WIDE1-1,WIDE2-1,BRASIL:!");
-      client.print(location);
-      client.print(sta_symbol);
-      client.print(comment);
-      Serial.println("Data sent OK");
+      // wait 2 seconds after press the beacon button, time to release
+      Serial.println("Send Beacon\n");
       delay(2000);
-      client.stop();
-      Serial.println("Server disconnected\n");
-      sent = true;
-
-      interval = (long)REPORT_INTERVAL * 60L * 1000L;
-      time_elapsed = 0;
-      //delay((long)REPORT_INTERVAL * 60L * 1000L);
-
-      //Serial.println(digitalRead(BT_BEACON));
-      while (time_elapsed < interval)
-      {
-        
-        if (!digitalRead(BT_BEACON)) break;
-        time_elapsed++;
-        delay(1);
-      }
-      time_elapsed = 0;
+      break;
     }
-    else
-    {
-      Serial.println("Login failed.");
-    }
-  }
-  else
-  {
-    Serial.println("Can not connect to the server.");
-  }
-  // If don't send data, reconect WiFi network
-  if (!sent) init_wifi();
+    time_elapsed++;
 
+  }
+  time_elapsed = 0;
 }
+
+/*********************************************************************************
+* Do not change anything below.
+*
+*/
 
 /* Connect to WiFi */
 void init_wifi()
@@ -161,6 +110,7 @@ void init_wifi()
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
+  WiFi.disconnect(); // Solve bug from reconect after reset button
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -173,6 +123,7 @@ void init_wifi()
   Serial.println(WiFi.localIP());
 }
 
+/* Wait response from server*/
 boolean wait4content(Stream* stream, char *target, int targetLen)
 {
   size_t index = 0;  // maximum target string length is 64k bytes!
@@ -221,3 +172,68 @@ boolean wait4content(Stream* stream, char *target, int targetLen)
   }
   return ret;
 }
+
+/*
+* Send station beacon
+*/
+unsigned char send_beacon(String user, String passwd, String location, String symbol, String comment)
+{
+  Serial.println("-----------------------------------------");
+  if ( client.connect(SVR_NAME, SVR_PORT) )
+  {
+    Serial.print("Conected on server: ");
+    Serial.println(SVR_NAME);
+
+    client.print("user ");
+
+    client.print(user);
+    client.print(" pass ");
+
+    client.print(passwd);
+    client.print(" vers APRS8266 ");
+    client.println(VER);
+    if ( wait4content(&client, SVR_VERIFIED, 8) )
+    {
+      // Insert your callsign
+      Serial.print("Login: ");
+      Serial.println(user);
+      client.print(user);
+
+      client.print(">APE001,TCPIP*,qAC,WIDE1-1,WIDE2-1,BRASIL:!");
+      // Insert your location
+      Serial.print("Location: ");
+      Serial.println(location);
+      client.print(location);
+      // Insert your station symbol
+      client.print(symbol);
+      // Insert your comment
+      Serial.print("Comment: ");
+      Serial.println(comment);
+      client.print(location);
+      // Do not change below
+      Serial.println("Data sent");
+      delay(2000);
+      client.stop();
+      Serial.println("Server disconnected");
+      Serial.println("-----------------------------------------\n");
+      delay(500);
+      return 1;
+    }
+    else
+    {
+      Serial.println("Login failed.");
+      Serial.println("-----------------------------------------\n");
+      init_wifi(); // Reconnect WiFi if occur any connection error
+      return 0;
+    }
+  }
+  else
+  {
+    Serial.println("Can not connect to the server.");
+    Serial.println("-----------------------------------------\n");
+    init_wifi(); // Reconnect WiFi if occur any connection error
+    return 0;
+  }
+  Serial.println("-----------------------------------------\n");
+}
+
